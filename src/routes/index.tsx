5a56d722151e86beac81c597faffd09712709d5b
kawabata324 <kawabataharuki48@gmail.com>
2022-12-10 23:44:59 +0900

    :sparkles: DoubleCounterとSingleCounterの作成

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { A } from 'solid-start'
-import Counter from '~/components/Counter'
+import SingleCounter from '~/components/SingleCounter'
+import DoubleCounter from '~/components/DoubleCounter'
 
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
-      <Counter />
+      <SingleCounter />
+      <DoubleCounter />
       <p class="mt-8">
         Visit{' '}
         <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
