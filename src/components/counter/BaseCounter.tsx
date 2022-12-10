fdac4aebbfd4583611f67d6b288d55c94b8a2e73
kawabata324 <kawabataharuki48@gmail.com>
2022-12-11 00:20:55 +0900

    :sparkles: 共通CounterComponent作成

---
+++ src/components/counter/BaseCounter.tsx
@@ -0,0 +1,18 @@
+import { Accessor } from 'solid-js'
+
+type Props = {
+  count: Accessor<number>
+  setCount: () => number
+  label: string
+}
+
+export default function BaseCounter(props: Props) {
+  return (
+    <button
+      class="w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
+      onClick={() => props.setCount()}
+    >
+      {props.label} {props.count()}
+    </button>
+  )
+}
