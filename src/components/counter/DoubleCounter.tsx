3c8963c86aa41bfec7d39a4061b247e07cb45acb
kawabata324 <kawabataharuki48@gmail.com>
2022-12-11 00:21:33 +0900

    :sparkles: 共通CounterComponentを使用

--- src/components/counter/DoubleCounter.tsx
+++ src/components/counter/DoubleCounter.tsx
@@ -1,13 +1,8 @@
 import { createSignal } from 'solid-js'
+import BaseCounter from '~/components/counter/BaseCounter'
 
 export default function DoubleCounter() {
   const [count, setCount] = createSignal(0)
-  return (
-    <button
-      class="w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
-      onClick={() => setCount(count() + 2)}
-    >
-      +2 {count()}
-    </button>
-  )
+  const countPlusTwo = () => setCount(count() + 2)
+  return <BaseCounter label="+2" count={count} setCount={countPlusTwo} />
 }
