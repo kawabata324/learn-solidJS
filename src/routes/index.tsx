d70c09f97ba87fa3f8ed91d4aa9adcf3dab524b1
kawabata324 <kawabataharuki48@gmail.com>
2022-12-12 00:07:07 +0900

    :sparkles: TopPageを編集

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,54 @@
-import { A } from 'solid-start'
-import SingleCounter from '~/components/counter/SingleCounter'
-import DoubleCounter from '~/components/counter/DoubleCounter'
-import counter from '~/store/counter'
+import { BLOG_TITLE } from '~/constants/title'
+import { createSignal, Match, onMount, Switch, Show } from 'solid-js'
+import { sleep } from '~/utils/sleep'
 
+type Status = 'pending' | 'loading' | 'done' | 'finished'
 export default function Home() {
-  const { count } = counter
+  const [code, setCode] = createSignal('')
+  const [status, setStatus] = createSignal<Status>('pending')
+  const TYPED_CODE = `cd ${BLOG_TITLE}`
+
+  onMount(async () => {
+    for (const s of TYPED_CODE) {
+      setCode(code() + s)
+      await sleep(200)
+    }
+    setStatus('loading')
+    await sleep(3000)
+    setStatus('done')
+    await sleep(500)
+    setStatus('finished')
+  })
+
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
-      <p class="text-lg my-4 text-blue-700l">{count()}</p>
-      <SingleCounter />
-      <DoubleCounter />
-      <p class="mt-8">
-        Visit{' '}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
-        </a>{' '}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {' - '}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{' '}
-      </p>
+      <Show
+        keyed={false}
+        when={status() !== 'finished'}
+        fallback={
+          <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
+        }
+      >
+        <div class="mockup-code text-left">
+          <Switch>
+            <Match when={status() === 'pending'} keyed={false}>
+              <pre data-prefix="$">
+                <code>{code()}</code>
+              </pre>
+            </Match>
+            <Match when={status() === 'loading'} keyed={false}>
+              <pre data-prefix=">" class="text-green-500">
+                <code>Loading...</code>
+              </pre>
+            </Match>
+            <Match when={status() === 'done'} keyed={false}>
+              <pre data-prefix=">">
+                <code>Done!</code>
+              </pre>
+            </Match>
+          </Switch>
+        </div>
+      </Show>
     </main>
   )
 }
