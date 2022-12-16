87869d0dccf61d92889b4a49b52687ee57542d14
kawabata324 <kawabataharuki48@gmail.com>
2022-12-16 21:37:30 +0900

    :fire: 不要なfileを削除

--- src/components/counter/DoubleCounter.tsx
+++
@@ -1,7 +0,0 @@
-import BaseCounter from '~/components/counter/BaseCounter'
-import counter from '~/store/counter'
-
-export default function DoubleCounter() {
-  const { countPlusTwo } = counter
-  return <BaseCounter label="+2" setCount={countPlusTwo} />
-}
